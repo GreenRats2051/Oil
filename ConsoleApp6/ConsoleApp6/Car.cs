9f664282808e7f7b7678df305a329abaf41645f9
ITHUB\Student <qew>
2024-11-18 19:14:49 +0300

    .

---
+++ ConsoleApp6/ConsoleApp6/Car.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Car : IVehiculo
+    {
+        private int fuel;
+
+        public Car(int initialFuel)
+        {
+            fuel = initialFuel;
+        }
+
+        public void Drive()
+        {
+            if (fuel > 0)
+            {
+                Console.WriteLine("Автомобиль находится в движении.");
+                fuel--;
+            }
+            else
+            {
+                Console.WriteLine("Недостаточно бензина для движения.");
+            }
+        }
+
+        public bool Refuel(int amount)
+        {
+            if (amount > 0)
+            {
+                fuel += amount;
+                Console.WriteLine($"Заправлено {amount} литров бензина. Текущий уровень бензина: {fuel} литров.");
+                return true;
+            }
+            Console.WriteLine("Количество бензина для заправки должно быть положительным.");
+            return false;
+        }
+    }
+}
---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car(0);
+
+            Console.Write("Введите количество бензина для заправки: ");
+            if (int.TryParse(Console.ReadLine(), out int fuelAmount))
+            {
+                myCar.Refuel(fuelAmount);
+            }
+            else
+            {
+                Console.WriteLine("Некорректный ввод. Пожалуйста, введите целое число.");
+            }
+            myCar.Drive();
+        }
+    }
+}
